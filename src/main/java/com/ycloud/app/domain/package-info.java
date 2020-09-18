48e7850921f8a30c9174dd3d2bd16487c17ee220
quintinchen <quintinchen@heytz.com>
2020-09-18 09:08:45 +0800

    update

---
+++ src/main/java/com/ycloud/app/domain/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * JPA domain objects.
+ */
+package com.ycloud.app.domain;
---
+++ src/test/java/com/ycloud/app/ArchTest.java
@@ -0,0 +1,29 @@
+package com.ycloud.app;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.core.importer.ImportOption;
+import org.junit.jupiter.api.Test;
+
+class ArchTest {
+
+    @Test
+    void servicesAndRepositoriesShouldNotDependOnWebLayer() {
+        JavaClasses importedClasses = new ClassFileImporter()
+            .withImportOption(ImportOption.Predefined.DO_NOT_INCLUDE_TESTS)
+            .importPackages("com.ycloud.app");
+
+        noClasses()
+            .that()
+            .resideInAnyPackage("com.ycloud.app.service..")
+            .or()
+            .resideInAnyPackage("com.ycloud.app.repository..")
+            .should()
+            .dependOnClassesThat()
+            .resideInAnyPackage("..com.ycloud.app.web..")
+            .because("Services and repositories should not depend on web layer")
+            .check(importedClasses);
+    }
+}
